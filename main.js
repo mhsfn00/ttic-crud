e180dad1327113c5ec5530b0c92e347f0423d79a
mhsfn00 <mhsfn00@gmail.com>
2025-05-07 14:58:50 -0300

    crud feito

--- main.js
+++ main.js
@@ -19,19 +19,15 @@ app.use(express.json());
 app.post('/user', async (req, res) => {
   try {
     const newUser = req.body;
-    const createdUser = await User.create({
+    const dbResponse = await User.create({
       username: newUser.username,
       tag: newUser.tag,
       bag: newUser.bag
     }).save();
 
-    res
-      .status(201)
-      .json({ message: 'Novo usuário salvo', novoUsuario: createdUser});
+    res.status(201).json({ resultado: dbResponse });
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: 'Erro ao salvar a pessoa', detalhes: err.message });
+    res.status(500).json({ erro: err.message });
   }
 });
 
@@ -39,13 +35,51 @@ app.post('/user', async (req, res) => {
 app.get('/user', async (req, res) => {
   try {
     const allUsers = await User.find();
-    res
-      .status(200)
-      .json({ usuarios: allUsers });
+
+    res.status(200).json({ resultado: dbResponse });
   } catch (err) {
-    res
-      .status(500)
-      .json({ error: 'Erro ao salvar a pessoa', detalhes: err.message });
+    res.status(500).json({ erro: err.message });
+  }
+});
+
+// PUT
+app.put('/user', async (req, res) => {
+  try {
+    const { userId } = req.body;
+    const { newValues } = req.body;
+
+    console.log( userId );
+    console.log( newValues );
+
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      throw new Error('Usuário não foi encontrado');
+    }
+    const dbResponse = await user.update(newValues).save();
+    
+    res.status(200).json({ resultado: dbResponse });
+  } catch (err) {
+    res.status(500).json({ erro: err.message });
+  }
+});
+
+// DELETE
+app.delete('/user', async (req, res) => {
+  try {
+    const { userId } = req.body;
+    if (!userId) {
+      throw new Error('id não fornecido');
+    }
+
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      throw new Error('Usuário não foi encontrado');
+    }
+    const dbResponse = await user.remove();
+    
+    res.status(200).json({ resultado: dbResponse });
+  } catch (err) {
+    res.status(500).json({ erro: err.message });
   }
 });
 
